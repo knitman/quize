b9ad002f1061b481b3c46f0e7e4d7fe6e44294ab
knitman <124901709+knitman@users.noreply.github.com>
2026-02-10 11:52:54 +0200

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,1 +1,6 @@
-<html><body><h1>Η ΓΝΩΣΗ ΕΙΝΑΙ ΝΙΚΗ</h1></body></html>
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()]
+});
